550e57d484ed92d2c17dc22ae6fdef2bdb45d1d8
PatS4050 <patrick@straatman.eu>
2026-04-08 21:00:15 +0200

    Opgave 2a under construction, worstel met het aanspsreken van een object ipv array

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
 import './App.css';
 import countSoldProducts from "./helpers/countSoldProducts.js";
-import {inventory} from "./constants/inventory.js";
+//deze is een default waarde//
+import {inventory, bestSellingTv} from "./constants/inventory.js";
 import {countBuyProducts} from "./helpers/countBuyProducts.js"
 import {countSellProducts} from "./helpers/countSellProducts.js";
+// deze zijn variabelen met export als pre//
+import {typeProducts} from "./helpers/informationProducts.js";
+
 
 function App() {
-// let countSellProducts = countBuyProducts(inventory) - countSoldProducts(inventory)
-// console.log(countSoldProducts(inventory))
 
-return (
-    <>
-        <h1>Begin hier met met maken van de applicatie!</h1>
-        <p>Totaal verkochte TV's <span className="greenText">{countSoldProducts(inventory)}</span></p>
-        <p>Totaal ingekochte TV's <span className="blueText">{countBuyProducts(inventory)}</span></p>
-        <p>Totaal nog te verkopen TV's <span className="redText"> {countSellProducts(inventory)}</span>
 
-        </p>
-    </>
 
-)
+
+
+
+    return (
+        <>
+            <h1>Begin hier met met maken van de applicatie!</h1>
+            <p>Totaal verkochte TV's <span className="greenText">{countSoldProducts(inventory)}</span></p>
+            <p>Totaal ingekochte TV's <span className="blueText">{countBuyProducts(inventory)}</span></p>
+            <p>Totaal nog te verkopen TV's <span className="redText"> {countSellProducts(inventory)}</span></p>
+            <section>
+                <h2>Type TV</h2>
+                <p>{typeProducts(bestSellingTv)}</p>
+                {/*// Hoe declareer ik hier een object ipv array //*/}
+                <p>{typeProducts(inventory)}</p>
+            </section>
+        </>
+    )
 }
 
 export default App
---
+++ src/helpers/informationProducts.js
@@ -0,0 +1,8 @@
+//.helpers
+export function typeProducts(tomato) {
+    let typeProduct = toString()
+    for (let i = 0; i < tomato.length; i++) {
+        typeProduct += tomato[i].type;
+    }
+return typeProduct
+}
