9dc408ee9ff76041c5b39b21768f53bc80c90212
PatS4050 <patrick@straatman.eu>
2026-04-10 17:29:25 +0200

    Opgave t/m 2c en beetje 2d

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,32 @@ import {inventory, bestSellingTv} from "./constants/inventory.js";
 import {countBuyProducts} from "./helpers/countBuyProducts.js"
 import {countSellProducts} from "./helpers/countSellProducts.js";
 // deze zijn variabelen met export als pre//
-import {brandProducts, typeProducts, nameProducts, priceProducts, screenSizesProducts} from "./helpers/informationProducts.js";
+import {
+    brandProducts,
+    typeProducts,
+    nameProducts,
+    priceProducts,
+    screenSizesProducts
+} from "./helpers/informationProducts.js";
 
 
 function App() {
 
 
-
-
-
-
     return (
         <>
-            <h1>Begin hier met met maken van de applicatie!</h1>
-            <p className="greenText">Totaal verkochte TV's {countSoldProducts(inventory)}</p>
-            <p className="blueText">Totaal ingekochte TV's {countBuyProducts(inventory)}</p>
-            <p className="redText">Totaal nog te verkopen TV's {countSellProducts(inventory)}</p>
-            <section>
-
-                <h2>Type TV</h2>
-                <p>{brandProducts(bestSellingTv)} {typeProducts(bestSellingTv)} - {nameProducts(bestSellingTv)}</p>
-                <p>€{priceProducts(bestSellingTv)},-</p>
-                <p>{screenSizesProducts(bestSellingTv)}</p>
-                {/*<p>{screenSizesProductsMm(bestSellingTv)}</p>*/}
-            </section>
+            <body>
+                <h1>Begin hier met met maken van de applicatie!</h1>
+                <p className="greenText">Totaal verkochte TV's {countSoldProducts(inventory)}</p>
+                <p className="blueText">Totaal ingekochte TV's {countBuyProducts(inventory)}</p>
+                <p className="redText">Totaal nog te verkopen TV's {countSellProducts(inventory)}</p>
+                <section className="bestSelling">
+                    <p className="typeTv">{brandProducts(bestSellingTv)} {typeProducts(bestSellingTv)} - {nameProducts(bestSellingTv)}</p>
+                    <h2>€{priceProducts(bestSellingTv)},-</h2>
+                    <p>{screenSizesProducts(bestSellingTv)}</p>
+                    {/*<p>{screenSizesProductsMm(bestSellingTv)}</p>*/}
+                </section>
+            </body>
         </>
     )
 }
