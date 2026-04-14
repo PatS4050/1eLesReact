98913485f3b0fa047854a2498510f89be0422ad7
PatS4050 <patrick@straatman.eu>
2026-04-14 19:51:38 +0200

    DEEL 2 - opgave 1b

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 } from "./helpers/informationProducts.js";
 import minus from "./assets/minus.png"
 import check from "./assets/check.png"
-import {showOutcomeInConsole} from "./constants/arrayOefening.js"
+// import {showOutcomeInConsole} from "./constants/arrayOefening.js"
 
 // import bestBeeldscherm from "https://w7.pngwing.com/pngs/870/575/png-transparent-samsung-ku6000-led-backlit-lcd-ultra-high-definition-television-4k-resolution-smart-tv-samsung-television-media-led.png"//
 
@@ -27,8 +27,7 @@ function App() {
     function buttonClick(e) {
         console.log(e)
     }
-    showOefeningOneInConsole();
-
+    // console.log(showOutcomeInConsole)
     return (
         <>
             <div className="body">
--- src/constants/arrayOefening.js
+++ src/constants/arrayOefening.js
@@ -1,17 +1,35 @@
 import {inventory} from './inventory.js';
+
+function showOutcomeInConsole() {
+
 //opgave 1a
+
 const typeTVs = inventory.map ((typeTV) => {
     // console.log(inventory.type);
     return typeTV.type;
 })
 console.log(typeTVs);
+
 //opgave 1b
-//een sortering
+//een sortering van alle uitverkochte tvs
+//heb een const nodig van alle ingekochte tvs
+//heb een const nodi van alle verkochte tvs
+//als de a-b =0 dan lijst van uitverkochte tvs
 
+// const inStockTVs = inventory.map((inStockTV) => {
+//     return inStockTV.originalStock;
+// })
+// const soldTVs = inventory.map((soldTV) => {
+//     return soldTV.sold;
+// })
+let soldOutTVs= inventory.map((stockTV)=>{
+    if (stockTV.originalStock - stockTV.sold === 0)
+    {return stockTV}
+})
 
-function showOutcomeInConsole() {
-    console.log(typeTVs);
-    console.log('uitkomst opdracht 1b');
+// console.log(inStockTVs)
+// console.log(soldTVs)
+console.log(soldOutTVs)
 }
 
 export default showOutcomeInConsole;
